4b351fef2f06b4aea9fc0b8596b7ee04d6aafa98
abiney1321 <abiney1321@gmail.com>
2025-02-12 21:13:42 +0000

    number of characters implemented

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,8 +1,8 @@
-export default function Stats() {
+export default function Stats({ numberOfCharacters }) {
   return (
     <section className="stats">
       <Stat number={0} label="Words" />
-      <Stat number={0} label="Characters" />
+      <Stat number={numberOfCharacters} label="Characters" />
       <Stat number={280} label="Instagram" />
       <Stat number={2200} label="Facebook" />
     </section>
