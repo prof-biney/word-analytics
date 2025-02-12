e99d0fdc9a6218dd68ddc5f3cde8e74b733952c0
abiney1321 <abiney1321@gmail.com>
2025-02-12 13:55:36 +0000

    components semantics

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,3 +1,8 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <small>&copy; Copyright by BlackGold. All rights reserved.</small>
+      <small>Last checked limits: 5days ago</small>
+    </footer>
+  );
 }
